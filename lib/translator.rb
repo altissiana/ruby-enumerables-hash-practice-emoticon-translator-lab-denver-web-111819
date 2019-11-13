2fafc9a6b90263e87efe22236a5e3b967d778a73
Tissiana Alves <altissiana@gmail.com>
2019-11-13 03:02:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(file_path)
   new_hash = {:get_meaning => {}, :get_emoticon => {}}
   emoticons.each_pair do |key, value|
     new_hash[:get_meaning][value[1]] = key
+    new_hash[:get_emoticon][value[0]] = value[1]
   end  
   new_hash
 end
