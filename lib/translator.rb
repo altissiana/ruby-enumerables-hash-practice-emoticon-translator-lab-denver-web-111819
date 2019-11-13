b45524f9f7d0f8ed4f216e7a757882e207319304
Tissiana Alves <altissiana@gmail.com>
2019-11-13 03:13:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  emoticons = load_library(file_path)
+  emoticons[:get_emoticon][emoticon]
 end
 
 def get_english_meaning
